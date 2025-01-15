56c5f5d6d51f431a65ca6e67c840779947f16cc7
Ivan Chen <ichen60@stuy.edu>
2025-01-14 21:31:28 -0500

    special attack implemented and overrided applyDamage to function properly with multiple shields

--- Guardian.java
+++ Guardian.java
@@ -1,11 +1,13 @@
 public class Guardian extends Adventurer{
   int shieldCount, maxShields;
+  int turnCount;
   //constructors
   public Guardian(String name, int health){
     super(name, health);
     shieldCount = 1;
     maxShields = 3;
-    setShield(true); // will need to override applydamage to be consistent
+    turnCount = 0;
+    setShield(true); 
   }
 
   public Guardian(String name){
@@ -33,7 +35,41 @@ public class Guardian extends Adventurer{
   public String attack(Adventurer other){
     int damage = (int)(Math.random()*4+5) * (1 * getStrength()/10);
     other.applyDamage(damage);
-    setTurns(getTurnCount() + 1);
-    return "did " + damage + " to "  + other; 
+    turnCount++;
+    if (turnCount >= 3){
+      if (shieldCount <= maxShields){
+        restoreSpecial(1);
+        turnCount %= 0;
+        return this + " attacked " + other + "and dealt " + damage + "damage. They have gained an additional shield.";
+      }
+      else{
+        turnCount = 3;
+      }
+    }
+    return this + " attacked " + other + "and dealt " + damage + "damage. They have " + (3 - turnCount) + "turns left until they gain another shield."; 
+  }
+
+  /*Consumes all shields. Deal 3-4 damage, and increase damage by 6 for each shield consumed.*/
+  public String specialAttack(Adventurer other){
+    if (shieldCount > 0){
+      int damage = ((int)(Math.random()+3) + (shieldCount * 6)) * (1 * getStrength()/10);
+      setSpecial(0);
+      other.applyDamage(damage);
+      return this + " crashed their shields into " + other + ", dealing " + damage + "damage. " + this + " has lost all their shields.";
+    }
+    else{
+      return "No shields available. Instead " + attack(other);
+    }
+  }
+
+  @Override // need to override so that multiple shields work.
+  public void applyDamage(int amount){
+    if (shieldCount > 0){
+      setShield(true);
+    }
+    else{
+      setShield(false);
+    }
+    super.applyDamage(amount);
   }
 }
